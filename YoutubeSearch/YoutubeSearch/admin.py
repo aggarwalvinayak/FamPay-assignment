4b535e38523593e5300113c2b325ec93cab1f264
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-08-24 22:15:41 +0530

    Add Dashboard in admin-panel

--- YoutubeSearch/YoutubeSearch/admin.py
+++ YoutubeSearch/YoutubeSearch/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Video
+from django.contrib.admin import ModelAdmin
 
-admin.site.register(Video)
+class VideoAdmin(ModelAdmin):
+    model = Video
+    list_display = ('title','description','publishDate','thumbnailURL','videoURL')
+    list_filter = ('title','description','publishDate','thumbnailURL','videoURL')
+    ordering = ['-publishDate']
+
+admin.site.register(Video,VideoAdmin)
